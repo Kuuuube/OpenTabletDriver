1b5604f048ba80baf3df4c458496b983ebf29b18
kuuube <hexagonisalie@gmail.com>
2021-06-24 18:01:04 -0400

    exclude bad reports first

--- OpenTabletDriver/Vendors/Wacom/IntuosV2ReportParser.cs
+++ OpenTabletDriver/Vendors/Wacom/IntuosV2ReportParser.cs
@@ -7,12 +7,10 @@ namespace OpenTabletDriver.Vendors.Wacom
     {
         public virtual IDeviceReport Parse(byte[] data)
         {
-            return data[0] switch
+            return data[1] switch
             {
-                0x2 => new IntuosV2TabletReport(data),
-                0x10 => new IntuosV2TabletReport(data),
-                0x3 => new IntuosV2AuxReport(data),
-                _ => new DeviceReport(data)
+                0xC2 => new IntuosV2Filter(data),
+                _ => new IntuosV2TabletReport(data)
             };
         }
     }
