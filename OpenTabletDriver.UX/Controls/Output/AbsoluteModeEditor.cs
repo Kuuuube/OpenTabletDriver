db8b68edeedc42d8c78e0c460d5cc987dfc56989
kuuuube <hexagonisalie@gmail.com>
2026-07-13 12:37:25 -0400

    Add debug stuff

--- OpenTabletDriver.UX/Controls/Output/AbsoluteModeEditor.cs
+++ OpenTabletDriver.UX/Controls/Output/AbsoluteModeEditor.cs
@@ -298,6 +298,13 @@ namespace OpenTabletDriver.UX.Controls.Output
                 var displays = DesktopInterop.VirtualScreen?.Displays.ToArray()
                     ?? throw new InvalidOperationException("Could not get VirtualScreen");
 
+                // account for monitor layouts with negative offsets (e.g. Wayland supports this)
+                // skip IVirtualScreen's as these tend to be normalized to 0,0, which may confuse these methods
+                float xOffset = displays.Where(d => d is not IVirtualScreen).MinBy(d => d.Position.X)?.Position.X
+                    ?? throw new InvalidOperationException("Unable to look up X offset");
+                float yOffset = displays.Where(d => d is not IVirtualScreen).MinBy(d => d.Position.Y)?.Position.Y
+                    ?? throw new InvalidOperationException("Unable to look up Y offset");
+
                 foreach (var display in displays)
                 {
                     subMenu.Items.Add(
@@ -319,6 +326,16 @@ namespace OpenTabletDriver.UX.Controls.Output
                                 else
                                 {
                                     virtualScreen = DesktopInterop.VirtualScreen;
+
+                                    Console.WriteLine("(virtualScreen.Position.X, virtualScreen.Position.Y): (" + virtualScreen.Position.X.ToString() + ", " + virtualScreen.Position.Y.ToString() + ")");
+                                    Console.WriteLine("(xOffset, yOffset): (" + xOffset.ToString() + ", " + yOffset.ToString() + ")");
+                                    Console.WriteLine("(display.Width, display.Height): (" + display.Width.ToString() + ", " + display.Height.ToString() + ")");
+                                    Console.WriteLine("(display.Position.X, display.Position.Y): (" + display.Position.X.ToString() + ", " + display.Position.Y.ToString() + ")");
+
+                                    Console.WriteLine("Result (current 0.6.x 1564de3): (" + (display.Position.X - xOffset + virtualScreen.Position.X + (display.Width / 2)).ToString() + ", " + (display.Position.Y - yOffset + virtualScreen.Position.Y + (display.Height / 2)).ToString() + ")");
+                                    Console.WriteLine("Result (method in PR currently as 2fb76a1): (" + (display.Position.X + virtualScreen.Position.X + (display.Width / 2)).ToString() + ", " + (display.Position.Y + virtualScreen.Position.Y + (display.Height / 2)).ToString() + ")");
+                                    Console.WriteLine("Result (method in other PR using calculated offsets): (" + (display.Position.X - xOffset + (display.Width / 2)).ToString() + ", " + (display.Position.Y - yOffset + (display.Height / 2)).ToString() + ")");
+
                                     this.Area.X = display.Position.X + virtualScreen.Position.X + (display.Width / 2);
                                     this.Area.Y = display.Position.Y + virtualScreen.Position.Y + (display.Height / 2);
                                 }
